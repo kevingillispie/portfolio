e4ce7726aeeae690037f03dd0cc6cb0d7da6a0b9
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2026-03-10 10:21:51 -0700

    Remove MDX, update headers

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,8 @@
-import createMDX from '@next/mdx'
-
-const withMDX = createMDX({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-    },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 
     trailingSlash: false,
-
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
-                    },
-                    {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
-                    },
-                ],
-            },
-        ]
-    },
 }
 
-export default withMDX(nextConfig)
+export default nextConfig
